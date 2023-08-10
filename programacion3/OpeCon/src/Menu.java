3d8d6edc5abef0af195494fbcc43c5cf6943b888
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2023-08-10 20:07:40 -0300

    Cambios desde acer

--- programacion3/OpeCon/src/Menu.java
+++ programacion3/OpeCon/src/Menu.java
@@ -10,6 +10,9 @@ public class Menu {
         Scanner scanner = new Scanner(System.in);
         boolean salir = false;
         while(!salir){
+            System.out.println("Ingrese el size del conjunto: ");
+            int n = scanner.nextInt();
+            Conjunto conjunto = new Conjunto(n);
             System.out.println("MENU:\n" +
                     "1. Agregar elemento\n" +
                     "2. Eliminar elemento\n" +
@@ -18,9 +21,6 @@ public class Menu {
                     "5. Verificar existencia de elemento\n" +
                     "6. Salir");
             int opcion = scanner.nextInt();
-            System.out.println("Ingrese el size del conjunto: ");
-            int n = scanner.nextInt();
-            Conjunto conjunto = new Conjunto(n);
             switch (opcion){
                 case 1:
                     int agregar = Integer.parseInt(JOptionPane.showInputDialog("Ingresar el nro a agregar: "));
