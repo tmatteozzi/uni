5e8e7549745510919937b5802be0a22db050a252
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2023-10-04 11:02:16 -0300

    Doc

--- programacion3/TAD_ArbolBinario/src/Main.java
+++ programacion3/TAD_ArbolBinario/src/Main.java
@@ -1,9 +1,10 @@
-// CONSIGNA:
-// Diseñe y construya usted su propia versión del TAD Árbol Binario cumpliendo con:
-    // Desarrollar una interfaz gráfica de usuario que maneje menú de Barras y/o botones gráficos.
-    // Documentar debidamente las líneas de código que por su complejidad así lo requieran.
+/***
+CONSIGNA:
+Diseñe y construya usted su propia versión del TAD Árbol Binario cumpliendo con:
+-Desarrollar una interfaz gráfica de usuario que maneje menú de Barras y/o botones gráficos.
+-Documentar debidamente las líneas de código que por su complejidad así lo requieran.
+ ***/
 
-// IMPORTS
 import java.util.Scanner;
 
 public class Main {
