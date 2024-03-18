54feda7a497dc46ee140424572f196c2a29c654d
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-03-18 11:35:22 -0300

    Cambios en estilos

--- ingenieria-de-software-2/gca/gca-frontend/src/styleClassNames.ts
+++ ingenieria-de-software-2/gca/gca-frontend/src/styleClassNames.ts
@@ -2,23 +2,21 @@ const mainDiv = 'container mx-auto p-4';
 
 const containerDiv = 'bg-white rounded-lg p-4';
 
-const listH1 = 'text-lg';
+const listH1 = 'font-medium text-lg';
 
-const listDiv =
-    'flex items-center justify-between border-b border-gray-300 py-2';
+const listDiv = 'flex items-center justify-between rounded-xl px-2 py-2 m-2';
+const listButton =
+    'px-4 py-0.5 bg-blue-500 text-white rounded-xl hover:bg-blue-600 focus:outline-none focus:bg-blue-600';
 
 const addButton =
     'px-4 py-1 bg-gray-500 text-white rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600';
-
-const listButton =
-    'px-4 py-1 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600';
 
 const mainH1 = 'text-2xl font-bold';
 const mainH2 = 'text-xl font-bold';
 
 export {
-    mainDiv,
     containerDiv,
+    mainDiv,
     mainH1,
     mainH2,
     listH1,
