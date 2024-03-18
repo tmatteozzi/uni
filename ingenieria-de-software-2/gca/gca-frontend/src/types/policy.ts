7c495862562c54c10db313df17da2ac8a9f146de
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-03-18 11:36:30 -0300

    Policy Type

---
+++ ingenieria-de-software-2/gca/gca-frontend/src/types/policy.ts
@@ -0,0 +1,9 @@
+export interface Policy {
+    id: number;
+    startDate: Date;
+    endDate: Date;
+    productName: string;
+    branchName: string;
+    companyName: string;
+    userId: number;
+}
