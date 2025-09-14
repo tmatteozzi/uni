6b975a705865c5f00624defda9495f84f5e1db07
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2025-09-14 16:32:37 -0300

    TP BDII

---
+++ bases-de-datos-2/trabajo-practico/init/00_init.sql
@@ -0,0 +1,16 @@
+-- Conectarse explícitamente a la base creada por POSTGRES_DB
+\connect bike_stores;
+
+\echo '>> [OLTP] creando tablas esquema bike_stores...'
+\i /sql/bikeStores/create_bike_stores_tables.sql
+
+\echo '>> [OLTP] cargando datos...'
+\i /sql/bikeStores/load_bike_stores_data.sql
+
+\echo '>> [dwh] creando dimensiones...'
+\i /sql/dwh/create_dimensions.sql
+
+\echo '>> [dwh] creando hechos...'
+\i /sql/dwh/create_facts.sql
+
+\echo '>> Inicialización completada.'
